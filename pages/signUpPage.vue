2d34ae460555a76bec581bf8de9ed1eee975175d
bp18052 <bp18052@shibaura-it.ac.jp>
2022-02-09 02:34:23 +0900

    delete SignFormComponent

--- pages/signUpPage.vue
+++ pages/signUpPage.vue
@@ -1,13 +1,44 @@
 <template>
   <v-contaier>
     <v-row justify="center" align="center">
-      <SignField ref="signData"/>
+      <v-col cols="12" sm='40' >
+          <v-form
+          ref="Mail"
+          v-model="valid"
+          lazy-validation
+          >
+          <v-text-field
+          dense
+          outlined
+          v-model="setMail"
+          label="Mail"
+          hide-details="auto"
+          :style="{background: $vuetify.theme.themes.dark.headBackgrouund}"
+          :rules="emailRules"
+          >
+          </v-text-field>
+          <v-text-field
+          dense
+          outlined
+          v-model="setPass"
+          label="Password"
+          hide-details="auto"
+          :append-icon="showPass ? 'mdi-eye' : 'mdi-eye-off'"
+          :type="showPass ? 'text' : 'password'"
+          :rules="passRules"
+          :style="{background: $vuetify.theme.themes.dark.headBackgrouund}"
+          @click:append= "showPass = !showPass"
+          class="mt-1"
+          >
+          </v-text-field>
+        </v-form>
+      </v-col>
       <v-col cols="12" justify="end">
         <v-btn
           elevation="8"
           @click="overlay = !overlay"
           opacity="0.5"
-          style="margin-top: 10px;"
+          class="mt-2"
           color="primary"
           :disabled="disabled"
         >Submit
@@ -33,17 +64,33 @@ export default {
   name: 'SignUpPage',
   data: () => ({
     overlay: false,
-    disabled: true
+    disabled: true,
+    valid: true,
+    setMail: '',
+    setPass: '',
+    success: false,
+    showPass: false,
+    submit: true,
+    emailRules: [
+      (v:string) => !!v || 'E-mail is required',
+      (v:string) => /.+@.+\..+/.test(v) || 'E-mail must be valid'
+    ],
+    passRules: [
+      (v:string) => !!v || 'Passward is required',
+      (v:string) => /.+@.+\..+/.test(v) || 'error message'
+    ]
   }),
   watch: {
-    overlay (val:any) {
+    overlay (val:boolean) {
       val && setTimeout(() => {
         this.overlay = false
       }, 2000)
-    },
-    disabled () {
-      this.disabled = this.$ref.signData.submit()
     }
-  }
+  },
+  methods: {
+    permitSubmit() {
+      this.this.$refs.form.validation()
+    }
+  },
 }
 </script>
